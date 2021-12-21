6b889dc811e0d594b395af836b7cdb902602df85
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2021-12-21 15:55:55 +0100

    datetime_to_seconds test. Everything moved to combitimetable

--- tests/test_combitabconvert.py
+++ tests/test_combitabconvert.py
@@ -1,6 +1,10 @@
 import pandas as pd
-from modelitool.combitabconvert import get_dymo_time_index
+
 from modelitool.combitabconvert import df_to_combitimetable
+from modelitool.combitabconvert import seconds_to_datetime
+from modelitool.combitabconvert import datetime_to_seconds
+
+from datetime import timedelta
 
 
 class TestCombitabconvert:
@@ -14,19 +18,19 @@ class TestCombitabconvert:
             {"dumb_column": [0] * time_index.shape[0]},
             index=time_index
         )
-        assert get_dymo_time_index(df) == [3600.0, 7200.0, 10800.0]
+        assert datetime_to_seconds(df.index) == [3600.0, 7200.0, 10800.0]
 
     def test_df_to_combitimetable(self, tmpdir):
         time_index = pd.date_range(
-                    "2021-01-01 01:00:00",
-                    freq="H",
-                    periods=3
-                )
+            "2021-01-01 01:00:00",
+            freq="H",
+            periods=3
+        )
         df = pd.DataFrame(
-                    {"dumb_column": [0] * time_index.shape[0],
-                     "dumb_column2": [1] * time_index.shape[0]},
-                    index=time_index
-                )
+            {"dumb_column": [0] * time_index.shape[0],
+             "dumb_column2": [1] * time_index.shape[0]},
+            index=time_index
+        )
 
         ref = (
             '#1 \n'
@@ -43,3 +47,18 @@ class TestCombitabconvert:
             contents = file.read()
 
         assert contents == ref
+
+    def test_seconds_to_datetime(self):
+        test_index = pd.Series([
+            timedelta(seconds=43200).total_seconds(),
+            timedelta(seconds=43500).total_seconds()
+        ])
+
+        expected_res = pd.to_datetime([
+            "2009-01-01 12:00:00",
+            "2009-01-01 12:05:00"
+        ])
+
+        res = seconds_to_datetime(test_index, 2009)
+
+        pd.testing.assert_index_equal(expected_res, res)
