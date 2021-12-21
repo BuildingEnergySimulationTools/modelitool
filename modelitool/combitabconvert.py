72a9e5d309aabaeb3e68782aa3457e5d97a0ddc7
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2021-12-21 15:39:20 +0100

    datetime_to_seconds replace get_dymo_time_index

--- modelitool/combitabconvert.py
+++ modelitool/combitabconvert.py
@@ -2,6 +2,23 @@ import datetime as dt
 import pandas as pd
 
 
+def seconds_to_datetime(index_second, ref_year):
+    since = dt.datetime(ref_year, 1, 1, tzinfo=dt.timezone.utc)
+    diff_seconds = index_second + since.timestamp()
+    return pd.DatetimeIndex(pd.to_datetime(diff_seconds, unit='s'))
+
+
+def datetime_to_seconds(index_datetime):
+    time_start = dt.datetime(
+        index_datetime[0].year, 1, 1, tzinfo=dt.timezone.utc)
+    new_index = index_datetime.to_frame().diff().squeeze()
+    new_index[0] = dt.timedelta(
+        seconds=index_datetime[0].timestamp() - time_start.timestamp()
+    )
+    sec_dt = [elmt.total_seconds() for elmt in new_index]
+    return list(pd.Series(sec_dt).cumsum())
+
+
 def get_dymo_time_index(df):
     """
     Return a list containing seconds since the beginning of the Year
@@ -27,7 +44,7 @@ def df_to_combitimetable(df, filename):
             line += f"\t({i + 1}){col}"
         file.write(f"{line} \n")
 
-        df.index = get_dymo_time_index(df)
+        df.index = datetime_to_seconds(df.index)
 
         file.write(df.to_csv(
             header=False,
