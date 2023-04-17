28a51275e0e2b91d6f2f6e3161f8229b78bee5ba
Tesshub <thubert@nobatek.inef4.com>
2023-04-17 10:18:56 +0200

    Addition of test for re2020_shower_distribution

--- tests/test_tsgenerator.py
+++ tests/test_tsgenerator.py
@@ -20,7 +20,7 @@ class TestDHWaterConsumption:
         assert round(dhw1.df_coefficient.loc["2023-08-26 20:00", 'coef'], 4) == round(1.392 * 0.72 * 1.02, 4)
         assert round(dhw1.df_coefficient.loc["2023-09-10 11:00", 'coef'], 4) == round(1.752 * 0.92 * 1.13, 4)
 
-        # #test RE2020
+        # test RE2020
         dhw2 = DHWaterConsumption(n_dwellings=50, method='RE2020')
         dhw2.get_coefficient_calc_from_period(start=start, end=end)
         assert round(dhw2.df_coefficient.loc["2023-04-05 00:00", 'coef'], 4) == round(0 * 0.95, 4)
@@ -33,12 +33,32 @@ class TestDHWaterConsumption:
         end = dt.datetime(2024, 10, 20, 1, 0, 0)
         df = dhw.costic_random_shower_distribution(start=start, end=end, seed=42)
 
-        # assert isinstance(df, pd.DataFrame)
-        # assert 'consoECS_COSTIC_random' in df.columns
-
-        # vérifier que la somme de la consommation d'eau aléatoire est égale à la consommation totale estimée
+        # check that the sum of the random water consumption distribution equals the total consumption
         total_consoECS_COSTIC = dhw.costic_shower_distribution(start=start, end=end)['consoECS_COSTIC'].sum()
         assert np.isclose(df['consoECS_COSTIC_random'].sum(), total_consoECS_COSTIC, rtol=0.05)
 
-        # # vérifier que la consommation d'eau aléatoire est supérieure à zéro
-        # assert (df['consoECS_COSTIC_random'] > 0).all()
+    def test_re2020_shower_distribution(self):
+        dhw = DHWaterConsumption(n_dwellings=50,
+                                 s_moy_dwelling=49.6,
+                                 s_tot_building=2480,
+                                 method="RE2020")
+        start = dt.datetime(2022, 1, 1, 0, 0, 0)
+        end = dt.datetime(2024, 10, 20, 1, 0, 0)
+        df = dhw.re2020_shower_distribution(start=start, end=end)
+
+        # check Wednesday in April
+        assert np.isclose(df.loc["2023-04-05 00:00", 'consoECS_RE2020'], 0, rtol=0.05)
+        # check Saturday in August
+        assert np.isclose(df.loc["2023-08-26 20:00", 'consoECS_RE2020'], 285.5, rtol=0.05)
+        # check Sunday in September
+        assert np.isclose(df.loc["2023-09-10 08:00", 'consoECS_RE2020'], 752.7, rtol=0.05)
+
+        dhw = DHWaterConsumption(n_dwellings=12,
+                                 s_moy_dwelling=72,
+                                 s_tot_building=1000,
+                                 method="RE2020")
+        df = dhw.re2020_shower_distribution(start=start, end=end)
+        #check Vshower bath
+
+        # check Sunday in September
+        assert np.isclose(df.loc["2023-09-10 08:00", 'consoECS_RE2020'], 261.3, rtol=0.05)
