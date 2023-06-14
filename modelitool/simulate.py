a641e08afe20f989b345f2ca5c0115a507b617f4
Tesshub <thubert@nobatek.inef4.com>
2023-06-14 12:03:13 +0200

    flake8, black. and pyupgrade check

--- modelitool/simulate.py
+++ modelitool/simulate.py
@@ -50,14 +50,14 @@ class Simulator:
                 "fileName": model_path.as_posix(),
                 "modelName": model_path.stem,
                 "lmodel": lmodel,
-                "variableFilter": "|".join(output_list)
+                "variableFilter": "|".join(output_list),
             }
         else:
             model_system_args = {
                 "fileName": package_path.as_posix(),
                 "modelName": model_path,
                 "lmodel": lmodel,
-                "variableFilter": "|".join(output_list)
+                "variableFilter": "|".join(output_list),
             }
 
         self.model = ModelicaSystem(**model_system_args)
@@ -125,23 +125,19 @@ class Simulator:
         # t2 = time()
         # print(f"Setting new parameters took {t2-t1}s")
 
-    def simulate(
-        self,
-        simflags=None
-    ):
+    def simulate(self, simflags=None):
         self.simflags = simflags
         if self.simulation_options["outputFormat"] == "csv":
             resultfile = "res.csv"
         else:
             resultfile = "res.mat"
         self.model.simulate(resultfile=resultfile, simflags=simflags)
-        self.resultfile=resultfile
+        self.resultfile = resultfile
 
     def get_results(self, index_datetime=True):
         # Modelica solver can provide several results for one timestep
         # Moreover variable timestep solver can provide messy result
         # Manipulations are done to resample the index and provide seconds
-
 
         if self.simulation_options["outputFormat"] == "csv":
             res = pd.read_csv(self._simulation_path / "res.csv", index_col=0)
@@ -151,7 +147,9 @@ class Simulator:
                 ["time"] + self.output_list, resultfile="res.mat"
             ).T
             res = pd.DataFrame(
-                sol_list[:, 1:], index=sol_list[:, 0].flatten(), columns=self.output_list
+                sol_list[:, 1:],
+                index=sol_list[:, 0].flatten(),
+                columns=self.output_list,
             )
             res.columns = self.output_list
 
